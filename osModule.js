93e62df14421ebf5f55ea09af6baeda103e7534f
CarlosBF <cborrellf@outlook.es>
2025-04-22 20:26:01 +0200

    Changed name from host to platform

--- osModule.js
+++ osModule.js
@@ -1,7 +1,7 @@
 const os = require('node:os')
 
 function getOSData() {
-    return `Nombre: ${os.hostname}\nTipo: ${os.type}\nVersión: ${os.version}\nArquitectura: ${os.arch}\nCPUs: ${os.cpus().length}\nMemoria total: ${os.totalmem/1000000} MB\nMemoria libre: ${os.freemem/1000000} MB\n`
+    return `Nombre: ${os.platform()}\nTipo: ${os.type()}\nVersión: ${os.version()}\nArquitectura: ${os.arch()}\nCPUs: ${os.cpus().length}\nMemoria total: ${os.totalmem()/1000000} MB\nMemoria libre: ${os.freemem()/1000000} MB\n`
 }
 
 module.exports = getOSData
